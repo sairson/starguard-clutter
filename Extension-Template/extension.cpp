cb2d5384b7d49e91ee26d236c21541351637f66a
SaiRson <74412075+sairson@users.noreply.github.com>
2022-09-21 16:39:08 +0800

    完善starguard extension插件模板并遵循sliver的插件规则

--- Extension-Template/extension.cpp
+++ Extension-Template/extension.cpp
@@ -5,7 +5,7 @@
 #include "starguard.h"
 
 
-BOOL MyFunction(IN char* Parameters, OUT char** Result, goCallback callback) {
+BOOL MyFunction(IN char* Parameters, IN int* ParameterSize, goCallback callback) {
 	std::string msg{ "You must provide a command.\n\t0 = stop\n\t1 = start\n\t2 = get logs" };
 	callback(msg.c_str(), msg.length());
 	std::string amsg{ "123123,4213123,421312312"};
@@ -14,7 +14,7 @@ BOOL MyFunction(IN char* Parameters, OUT char** Result, goCallback callback) {
 }
 
 
-extern "C" BOOL __declspec(dllexport) Load(IN char* Parameters, OUT char** Result, goCallback callback)
+extern "C" BOOL __declspec(dllexport) Load(IN char* Parameters, IN int* ParameterSize, goCallback callback)
 {
-	return MyFunction(Parameters, Result,callback);
+	return MyFunction(Parameters, ParameterSize,callback);
 }
