e703d91539a1c97d121248855f3f617b83cd8621
SaiRson <74412075+sairson@users.noreply.github.com>
2022-09-21 16:36:46 +0800

    遵循sliver的插件调用格式，完善starguard的插件模板

--- Extension-Template/extension.cpp
+++ Extension-Template/extension.cpp
@@ -5,14 +5,16 @@
 #include "starguard.h"
 
 
-BOOL MyFunction(IN char* Parameters, OUT char** Result) {
-	StarGuardPrintln(Result, "[+] Init a Module Extension");
-	StarGuardPrintln(Result, "[+] Sleep 5 time Seconds");
+BOOL MyFunction(IN char* Parameters, OUT char** Result, goCallback callback) {
+	std::string msg{ "You must provide a command.\n\t0 = stop\n\t1 = start\n\t2 = get logs" };
+	callback(msg.c_str(), msg.length());
+	std::string amsg{ "123123,4213123,421312312"};
+	callback(amsg.c_str(), amsg.length());
 	return TRUE;
 }
 
 
-extern "C" BOOL __declspec(dllexport) Load(IN char* Parameters, OUT char** Result)
+extern "C" BOOL __declspec(dllexport) Load(IN char* Parameters, OUT char** Result, goCallback callback)
 {
-	return MyFunction(Parameters, Result);
+	return MyFunction(Parameters, Result,callback);
 }
--- Extension-Template/starguard.h
+++ Extension-Template/starguard.h
@@ -1,5 +1,4 @@
 #pragma once
 #include<string>
 
-// 一个将控制台输出重定向到结果集的函数
-void StarGuardPrintln(char** result, std::string in); 
+typedef int (*goCallback)(const char*, int);
